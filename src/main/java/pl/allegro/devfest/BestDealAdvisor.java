3a7ded7897b0dcffe5f5cd7edf0d931ff0962f2c
wojtek-szymanski <wk.szymanski@gmail.com>
2014-11-08 16:00:07 +0100

    Best deal implementation

--- src/main/java/pl/allegro/devfest/BestDealAdvisor.java
+++ src/main/java/pl/allegro/devfest/BestDealAdvisor.java
@@ -1,8 +1,12 @@
 package pl.allegro.devfest;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Optional;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class BestDealAdvisor {
 
@@ -13,6 +17,30 @@ public class BestDealAdvisor {
     }
 
     public Optional<BestDeal> findBestDeal(DealCriteria dealRequirements) {
-        throw new UnsupportedOperationException("Implement me!");
+        Set<GoodType> expectedGoodTypes = dealRequirements.getGoodTypes();
+        Map<GoodType, List<Auction>> auctionsByType = auctions.stream().
+                filter(t -> expectedGoodTypes.contains(t.getGoodType())).
+                collect(Collectors.groupingBy(Auction::getGoodType));
+
+        Set<GoodType> actualGoodTypes = auctionsByType.keySet();
+        if (!actualGoodTypes.containsAll(expectedGoodTypes)) {
+            return Optional.empty();
+        }
+
+
+        List<Auction> result = new ArrayList<>();
+        auctionsByType.forEach(new BiConsumer<GoodType, List<Auction>>() {
+            @Override
+            public void accept(GoodType goodType, List<Auction> auctions) {
+                Optional<Auction> min = auctions.stream().min(Comparator.comparing(price -> price.getPrice()));
+                result.add(min.get());
+            }
+        });
+
+        BestDeal bestDeal = new BestDeal(result);
+        BigDecimal totalPrice = bestDeal.getTotalPrice();
+        BigDecimal budget = dealRequirements.getBudget();
+
+        return totalPrice.compareTo(budget) <= 0 ? Optional.of(bestDeal) : Optional.empty();
     }
 }
